687e08a23ac0166abe4d7280a17a7049de2ce474
鄭秉正 <126307842+mattchengg@users.noreply.github.com>
2025-04-14 15:18:27 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,17 +1,17 @@
 SKIPUNZIP=1
 
 echo "Moving vendor/tee to vendor/tee_asia"
-mv -f "$WORK_DIR/vendor/tee" "$WORK_DIR/vendor/tee_asia"
+mv -f "$WORK_DIR/vendor/tee" "$WORK_DIR/vendor/tee_sea"
 mkdir -p "$WORK_DIR/vendor/tee" 
 
 echo "Moving firmware files to asia directory"
 for file in AIE.bin mfc_fw.bin pablo_icpufw.bin calliope_sram.bin os.checked.bin vts.bin; do
-  mkdir -p "$WORK_DIR/vendor/firmware/asia"
-  mv -f "$WORK_DIR/vendor/firmware/$file" "$WORK_DIR/vendor/firmware/asia"
+  mkdir -p "$WORK_DIR/vendor/firmware/sea"
+  mv -f "$WORK_DIR/vendor/firmware/$file" "$WORK_DIR/vendor/firmware/sea"
   touch "$WORK_DIR/vendor/firmware/$file"
 done
 
-for region in eur sea; do
+for region in asia eur sea; do
   echo "Copying files for $region regions"
   cp -a --preserve=all "$SRC_DIR/target/a54x/patches/vendor/vendor/tee_$region" "$WORK_DIR/vendor"
   for file in AIE.bin mfc_fw.bin pablo_icpufw.bin calliope_sram.bin os.checked.bin vts.bin; do
